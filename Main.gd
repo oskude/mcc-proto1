244bbfc99df5d51278fcb63b41084a7e185647c1
Andre Schmidt <andre.osku.schmidt@gmail.com>
2018-10-04 20:50:27 +0200

    show fps

--- Main.gd
+++ Main.gd
@@ -1,9 +1,12 @@
 extends Spatial
 
+onready var fps = get_node("fps")
+
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _process(delta):
+	fps.set_text(str(Engine.get_frames_per_second()))
 	if Input.is_action_just_pressed("quit_game"):
 		get_tree().quit()
 	elif Input.is_action_just_pressed("reload_scene"):
