597ed4657a6a20adc81bb309f84e135ff76fdc1c
Andre Schmidt <andre.osku.schmidt@gmail.com>
2018-10-05 15:58:54 +0200

    fixes #1 vertical mouse code is all wrong...

--- Player.gd
+++ Player.gd
@@ -11,18 +11,23 @@ var yaw = 0
 var pitch = 0
 var velocity = Vector3()
 
+onready var Head = $Head
+onready var Camera = $Head/Camera
+
 func _input(event):
 	if event is InputEventMouseMotion:
-		$Head.rotate_y(deg2rad(-event.relative.x * mouse_speed))
-		$Head/Camera.rotate_x(clamp(deg2rad(-event.relative.y * mouse_speed), -90, 90))
+		Head.rotate_y(deg2rad(-event.relative.x * mouse_speed))
+		Camera.rotate_x(deg2rad(-event.relative.y * mouse_speed))
+		Camera.rotation_degrees.x = clamp(Camera.rotation_degrees.x, -89.9, 89.9)
 
 func _physics_process(delta):
 	var direction = Vector3()
-	var aim = $Head/Camera.get_global_transform().basis
+	var aim = Camera.get_global_transform().basis
 	if Input.is_action_pressed("player_move_forward"):  direction -= aim.z
 	if Input.is_action_pressed("player_move_backward"): direction += aim.z
 	if Input.is_action_pressed("player_move_left"):     direction -= aim.x
 	if Input.is_action_pressed("player_move_right"):    direction += aim.x
+	direction.y = 0
 	direction = direction.normalized()
 	
 	var gravity = velocity.y
@@ -30,8 +35,8 @@ func _physics_process(delta):
 		gravity += GRAVITY * delta
 	
 	var accel = DEACCEL if direction.length() > 0 else ACCEL
-	velocity = velocity.linear_interpolate(direction * MAX_SPEED, accel * delta);
-	velocity.y = gravity;
+	velocity = velocity.linear_interpolate(direction * MAX_SPEED, accel * delta)
+	velocity.y = gravity
 	
 	var motion = velocity * delta
 	motion = move_and_slide(motion, UP)
